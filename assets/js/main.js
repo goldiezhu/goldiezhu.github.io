f6bdcc1fd96fd3d352c03dc7850f22d27cc15aeb
goldiezhu <37753494+goldiezhu@users.noreply.github.com>
2021-01-07 21:18:26 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,15 +73,4 @@
 			});
 
 		}
-	$(window).load(function () {
-	$(".trigger_popup_fricc").click(function(){
-        $('.hover_bkgr_fricc').show();
-  	  });
-  	$('.hover_bkgr_fricc').click(function(){
-        $('.hover_bkgr_fricc').hide();
-   	});
-   	$('.popupCloseButton').click(function(){
-        $('.hover_bkgr_fricc').hide();
-   	});
-	});
 })(jQuery);
