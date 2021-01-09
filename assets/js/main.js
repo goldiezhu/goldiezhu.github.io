bc6765f56a777f0bc5b05fadfb90d63e9f991424
goldiezhu <37753494+goldiezhu@users.noreply.github.com>
2021-01-08 17:19:19 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,5 +72,19 @@
 				leave:		function() { $header.removeClass('alt'); }
 			});
 		}
-	
+		$window.scroll(function() {
+		    var height = $(window).scrollTop();
+		    if (height > 100) {
+			$('#back2Top').fadeIn();
+		    } else {
+			$('#back2Top').fadeOut();
+		    }
+		});
+		$(document).ready(function() {
+		    $("#back2Top").click(function(event) {
+			event.preventDefault();
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			return false;
+		    });
+		});
 })(jQuery);
